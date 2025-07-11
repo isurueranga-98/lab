f75e00fa5538eb688bdbec2525c512e44e2a8a81
isurueranga-98 <isurueranga.0@gmail.com>
2025-07-11 21:21:06 +0530

    Fix empty saved-reports page.tsx file - add basic page component

---
+++ app/saved-reports/page.tsx
@@ -0,0 +1,14 @@
+import { PageContainer } from "@/components/layout/page-container.layout";
+import React from "react";
+
+const SavedReportsPage = (): React.JSX.Element => {
+  return (
+    <PageContainer title="Saved Reports">
+      <div>
+        <p>Saved reports will be displayed here.</p>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default SavedReportsPage;
